74606b7bd14576f4a04943f471ef8d8cd729a79f
ajay_pthade2595 <138894609+ajay-collo@users.noreply.github.com>
2026-09-20 23:54:31 +0530

    Add files via upload

---
+++ check_caseUL.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+  
+  int main(){
+   char ch;
+   cout << "enter char : ";
+   cin >> ch;
+ 
+    if(ch >= 'a' && ch <= 'z') {
+      cout << "lowercase";
+    }
+
+    else{
+      cout << "uppercase";
+    }
+  
+  return 0;
+
+  }
